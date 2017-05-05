3c72eea681fe7b3ef9de4fce6edf4dfc9f1ec9d8
aidanlopez <ALopez7988@student.pps.net>
2017-05-05 12:57:18 -0700

    too many updates at once..
    blah

---
+++ ddccode1/matrix1.bat
@@ -0,0 +1,19 @@
+@echo off
+
+rem turns off notifications
+
+color 1F
+
+rem this makes it look like the blue screen of death
+
+:loop1
+
+rem this createsan infinite loop
+
+echo %random% Blue %random% screen %random death %random
+
+rem this puts words and scary stuff on the screen
+
+goto loop1
+
+rem this makes it return to the infinite loop
